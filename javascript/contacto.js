f2dab0393bdf26e7b7453301e5e017434a2e81ae
emanuelromano <eromanox@gmail.com>
2023-10-21 13:08:18 -0300

    Cambios en Sobre Nosotros y Contacto

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -9,7 +9,43 @@ function cargarContadorCarrito() {
     }
 }
 
+// Ocultar elementos del Nav Bar en modo para moviles
+function menuHamburguesa() {
+    let menuItemDisplay = window.getComputedStyle(document.querySelector('#menu-item-1')).display
+
+    if (menuItemDisplay === "flex") {
+        document.getElementById("menu-item-1").style.display = "none"
+        document.getElementById("menu-item-2").style.display = "none"
+        document.getElementById("menu-item-3").style.display = "none"
+        document.getElementById("menu-item-4").style.display = "none"
+    } else if (menuItemDisplay === "none") {
+        document.getElementById("menu-item-1").style.display = "flex"
+        document.getElementById("menu-item-2").style.display = "flex"
+        document.getElementById("menu-item-3").style.display = "flex"
+        document.getElementById("menu-item-4").style.display = "flex"
+    }
+}
+
 // Funciones a ejecutarse al cargar completamente la página
 window.addEventListener('load', function () {
     cargarContadorCarrito()
 })
+
+// Ocultar elementos del Nav Bar en modo para moviles con Event Listener
+function tamañoPantalla() {
+    let tamaño = document.documentElement.clientWidth
+
+    if (tamaño > 800) {
+        document.getElementById("menu-item-1").style.display = "flex"
+        document.getElementById("menu-item-2").style.display = "flex"
+        document.getElementById("menu-item-3").style.display = "flex"
+        document.getElementById("menu-item-4").style.display = "flex"
+    } else if (tamaño <= 800) {
+        document.getElementById("menu-item-1").style.display = "none"
+        document.getElementById("menu-item-2").style.display = "none"
+        document.getElementById("menu-item-3").style.display = "none"
+        document.getElementById("menu-item-4").style.display = "none"
+    }
+}
+
+window.addEventListener('resize', tamañoPantalla)
