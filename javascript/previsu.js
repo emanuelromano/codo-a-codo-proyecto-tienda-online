06fb73840df7a0318c0e589b95760bf89c1afd69
emanuelromano <eromanox@gmail.com>
2023-12-04 17:28:50 -0300

    Cambios JS

--- javascript/previsu.js
+++ javascript/previsu.js
@@ -7,10 +7,6 @@ function mostrarPrevisualizacion() {
         var preview = document.getElementById('previsualizacion');
         preview.innerHTML = '<img src="' + url + '" alt="Previsualización">';
     } else {
-<<<<<<< HEAD
         alert('Por favor, ingrese una URL de imágen válida.');
-=======
-        alert('Link invalido, intente nuevamente con otro link');
->>>>>>> e2a4af2b2b1ca288d2cd4400126b3da6e2d41013
     }
 }
